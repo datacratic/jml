5409b9889fecad73bb558f305fabdef1f836da62
Wolfgang Sourdeau <wolfgang.sourdeau@iperceptions.com>
2017-08-18 21:18:25 -0400

    build lz4cli program after setting compile options

--- lz4/lz4cli/lz4cli.mk
+++ lz4/lz4cli/lz4cli.mk
@@ -1,3 +1,3 @@
 LZ4CLI_SOURCES = lz4cli.c lz4io.c bench.c datagen.c
-$(eval $(call program,lz4cli,lz4,$(LZ4CLI_SOURCES)))
 $(eval $(call set_compile_option,$(LZ4CLI_SOURCES),-Ijml/lz4))
+$(eval $(call program,lz4cli,lz4,$(LZ4CLI_SOURCES)))
